LineageOS/android_hardware_qcom_display

display: msm8994: replace %zd with %ld for ssize_t argument
Change-Id: I3a3fe90d6700062aaea3584cc4893845648c580b

msm8994/libhdmi/hdmi.cpp
@@ -248,7 +248,7 @@ void HDMIDisplay::readCEUnderscanInfo()
         return;
     } else {
         len = read(hdmiScanInfoFile, scanInfo, sizeof(scanInfo)-1);
-        ALOGD("%s: Scan Info string: %s length = %zu",
+        ALOGD("%s: Scan Info string: %s length = %lu",
                  __FUNCTION__, scanInfo, len);
         if (len <= 0) {
             close(hdmiScanInfoFile);
@@ -363,7 +363,7 @@ bool HDMIDisplay::readResolution()
         return false;
     } else {
         len = read(hdmiEDIDFile, edidStr, sizeof(edidStr)-1);
-        ALOGD_IF(DEBUG, "%s: EDID string: %s length = %zu",
+        ALOGD_IF(DEBUG, "%s: EDID string: %s length = %lu",
                  __FUNCTION__, edidStr, len);
         if (len <= 0) {
             ALOGE("%s: edid_modes file empty", __FUNCTION__);

OzzysCmAcc/cxy

libhdmi/hdmi.cpp
@@ -193,7 +193,7 @@ void HDMIDisplay::readCEUnderscanInfo()
         return;
     } else {
         len = read(hdmiScanInfoFile, scanInfo, sizeof(scanInfo)-1);
-        ALOGD("%s: Scan Info string: %s length = %zd",
+        ALOGD("%s: Scan Info string: %s length = %ld",
                  __FUNCTION__, scanInfo, len);
         if (len <= 0) {
             close(hdmiScanInfoFile);
@@ -317,7 +317,7 @@ bool HDMIDisplay::readResolution()
         return false;
     } else {
         len = read(hdmiEDIDFile, edidStr, sizeof(edidStr)-1);
-        ALOGD_IF(DEBUG, "%s: EDID string: %s length = %zd",
+        ALOGD_IF(DEBUG, "%s: EDID string: %s length = %ld",
                  __FUNCTION__, edidStr, len);
         if (len <= 0) {
             ALOGE("%s: edid_modes file empty", __FUNCTION__);